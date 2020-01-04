e9f9f8c248a906bc7b999d059c658dce826127a9
Henrik Larsen <henrikvlarsen@hotmail.com>
2020-01-04 01:19:37 +0100

    SSH step added to my KubeJoin.ps1 script. The official KubeCluster.ps1 script only prints intstructions if running with -install flag and public key is already present

--- KubeJoin.ps1
+++ KubeJoin.ps1
@@ -1,5 +1,21 @@
 #based on https://kubernetes.io/docs/setup/production-environment/windows/user-guide-windows-nodes/
 #execute this from the .\sig-windows-tools-master\kubeadm dir 
 
-#must be executed using Powershell 5.1 - not Powershell core as it will break in get-windowsfeature somewhere..
-.\KubeCluster.ps1 -ConfigFile .\KubeClustervxlan.hvl71.json -join
+$pubKey=$(cat $env:userprofile/.ssh/id_rsa.pub)
+        Write-Host "Execute the below commands on the Linux control-plane node to add this Windows node's public key to its authorized keys"
+        Write-Host "touch ~/.ssh/authorized_keys"
+        Write-Host "echo $pubKey >> ~/.ssh/authorized_keys"
+
+Read-Host "Press any key to continue on the windows node - when the above commnds have been executed on master node.."
+Write-Host "To verify ssh connectivity from the Windows node now do:" 
+Write-Host "ssh <master-node-user>@<master-node-ip>"
+Write-Host "If connection succeeds - we should be good to go"
+
+$answer=Read-Host "Confirm you want to proceed and join this node to the cluster. Type 'c' to confirm"
+if($answer -eq "c"){
+    Write-Host "Starting .\KubeCluster.ps1 -ConfigFile .\KubeClustervxlan.hvl71.json -join"
+    #must be executed using Powershell 5.1 - not Powershell core as it will break in get-windowsfeature somewhere..
+    .\KubeCluster.ps1 -ConfigFile .\KubeClustervxlan.hvl71.json -join
+} else {
+    Write-Host "OK - aborting.."
+}
